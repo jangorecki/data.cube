0ccc5ed540576d678540c70107d7af6f7414a104
jangorecki <j.gorecki@wit.edu.pl>
2015-11-04 19:02:43 +0000

    misc

--- R/datatable.R
+++ R/datatable.R
@@ -1,9 +1,8 @@
-as.data.table.array = function(x, na.rm=TRUE){
+as.data.table.array = function(x, na.rm=TRUE, ...){
     dimnms = names(dimnames(x))
     if("value" %in% dimnms) stop("Array to convert must not already have `value` character as dimension name. `value` name is reserved to converted measure, rename dimname of input array.")
     r = do.call(CJ, dimnames(x))[, .(value = eval(as.call(lapply(c("[","x", dimnms), as.symbol)))),, keyby = c(dimnms)]
-    if(na.rm) r = r[!is.na(value)]
-    r
+    if(na.rm) r[!is.na(value)] else r
 }
 
 as.array.data.table = function(x, dimnames, measure, ...){

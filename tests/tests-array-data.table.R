b681f27a6c3fce89636eeab0471e840e938bb4e8
jangorecki <j.gorecki@wit.edu.pl>
2015-11-11 01:54:54 +0000

    rename overlapping symbol

--- tests/tests-array-data.table.R
+++ tests/tests-array-data.table.R
@@ -3,7 +3,7 @@ library(data.cube)
 
 X = populate_star(1e5, Y = 2015)
 cb = as.cube(X)[,,,,as.Date("2015-01-15")] # drop time dimension to fit array into memory
-dimnames = dimnames(cb)
+cb.dimnames = dimnames(cb)
 measure = "value"
 dt = as.data.table(cb)
 ar = as.array(cb, measure = measure)
@@ -11,7 +11,7 @@ dimcols = c(product = "prod_name", customer = "cust_profile", currency = "curr_n
 
 # length of array results, dummy tests
 stopifnot(
-    prod(sapply(dimnames, length)) > 0L,
+    prod(sapply(cb.dimnames, length)) > 0L,
     dt[, prod(sapply(.SD, uniqueN)), .SDcols = dimcols] > 0L
 )
 
@@ -25,20 +25,20 @@ stopifnot(all.equal(ar, as.array(dt, dimcols, measure)))
 
 # use dimcols unnamed
 ar.ren = ar
-dimnames(ar.ren) = setNames(dimnames, dimcols)
+dimnames(ar.ren) = setNames(cb.dimnames, dimcols)
 stopifnot(all.equal(ar.ren, as.array(dt, unname(dimcols), measure)))
 
 # use dimnames only
 dt.ren = copy(dt)
 setnames(dt.ren, dimcols, names(dimcols))
-stopifnot(all.equal(ar, as.array(dt.ren, measure = measure, dimnames = dimnames)))
+stopifnot(all.equal(ar, as.array(dt.ren, measure = measure, dimnames = cb.dimnames)))
 
 # use dimnames and dimcols
-stopifnot(all.equal(ar, as.array(dt, dimcols = dimcols, measure = measure, dimnames = dimnames)))
+stopifnot(all.equal(ar, as.array(dt, dimcols = dimcols, measure = measure, dimnames = cb.dimnames)))
 
 # detect measure
 stopifnot(
     all.equal(ar, as.array(dt[, c(dimcols, measure), with=FALSE], dimcols))
-    , all.equal(ar, as.array(dt.ren[, c(names(dimcols), measure), with=FALSE], dimnames = dimnames))
-    , all.equal(ar, as.array(dt[, c(dimcols, measure), with=FALSE], dimcols = dimcols, dimnames = dimnames))
+    , all.equal(ar, as.array(dt.ren[, c(names(dimcols), measure), with=FALSE], dimnames = cb.dimnames))
+    , all.equal(ar, as.array(dt[, c(dimcols, measure), with=FALSE], dimcols = dimcols, dimnames = cb.dimnames))
 )

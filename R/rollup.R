159bc9e586d2f350c2d34826ad84e1f8f09dd8dd
jangorecki <j.gorecki@wit.edu.pl>
2015-11-17 02:56:40 +0000

    rollup methods for cube and data.table

---
+++ R/rollup.R
@@ -0,0 +1,74 @@
+# method for data.table and cube
+rollup = function(x, ...){
+    UseMethod("rollup")
+}
+
+# rollup on data.table, man: http://stackoverflow.com/a/32938770/2490497
+rollup.data.table = function(x, j, by, .SDcols, levels=TRUE){
+    stopifnot(is.data.table(x), is.character(by), length(by) >= 2L)
+    stopifnot(is.logical(levels) | is.numeric(levels))
+    stopifnot(!"level" %in% by) # reserved
+    if(is.logical(levels)){
+        level = levels
+        levels = c(0L,seq_along(by))
+    } else if(is.numeric(levels)){
+        levels = as.integer(levels)
+        level = TRUE
+    }
+    if(missing(.SDcols)) .SDcols = character()
+    j = substitute(j)
+    aggrs = rbindlist(c(
+        lapply(1:(length(by)-1L), function(i){
+            if(i %in% levels){
+                if(length(.SDcols)) x[, eval(j), c(by[1:i]), .SDcols = .SDcols][, (by[-(1:i)]) := NA][, c("level") := i]
+                else x[, eval(j), c(by[1:i])][, (by[-(1:i)]) := NA][, c("level") := i]
+            }
+        }), # subtotals
+        list({
+            if(0L %in% levels){
+                if(length(.SDcols)) x[, eval(j), c(by), .SDcols = .SDcols][, c("level") := 0L]
+                else x[, eval(j), c(by)][, c("level") := 0L]
+            }
+        }), # leafs aggregations
+        list({
+            if(length(by) %in% levels){
+                if(length(.SDcols)) x[, eval(j), .SDcols = .SDcols][, c(by) := NA][, c("level") := length(by)]
+                else x[, eval(j)][, c(by) := NA][, c("level") := length(by)]
+            }
+        }) # grand total
+    ), use.names = TRUE, fill = FALSE)
+    if(!level) aggrs[, c("level") := NULL] else by = c(by, "level")
+    setcolorder(aggrs, neworder = c(by, names(aggrs)[!names(aggrs) %in% by]))
+    return(aggrs[])
+}
+
+# x cube
+# MARGIN character vector to rollup `by`
+# INDEX apply filter on levels where level 0 represents the most granular data, each next is an higher level aggregation. Having value of NULL and `length > 1L` will result double counting of measures, NULL recycles to length of dims.
+# FUN function to apply on all measures
+# ... more arguments passed to FUN
+# j call passed to data.table fact table.
+rollup.cube = function(x, MARGIN, INDEX = NULL, FUN, ..., j, drop = TRUE){
+    keys = x$dapply(key, simplify = TRUE)
+    measures = setdiff(names(x$env$fact[[x$fact]]), keys)
+    levels = if(!is.null(INDEX)) INDEX else c(0L,seq_along(MARGIN))
+    r = new.env()
+    if(missing(FUN)){
+        r$fact[[x$fact]] = eval(substitute(rollup(x = as.data.table(x), j = jj, by = MARGIN, levels = levels), env = list(jj = substitute(j))))
+    } else {
+        jj = as.call(list(quote(lapply), X = quote(.SD), FUN = substitute(FUN), "..." = ...))
+        r$fact[[x$fact]] = eval(substitute(rollup(x = as.data.table(x), j = jj, by = MARGIN, .SDcols = measures, levels = levels), env = list(jj = jj)))
+    }
+    new.fact.colnames = names(r$fact[[x$fact]])
+    new.fact.keys = setdiff(new.fact.colnames, measures)
+    dimcolnames = x$dapply(colnames)
+    new.dims = sapply(dimcolnames, function(dimcols) any(new.fact.keys %in% dimcols))
+    new.dims = names(new.dims)[new.dims]
+    names(new.fact.keys) = new.dims
+    r$dims = lapply(selfNames(new.dims), function(dim){
+        r$dims[[dim]] = setkeyv(unique(x$env$dims[[dim]], by = new.fact.keys[dim]), new.fact.keys[dim])[]
+    })
+    r$dims[["level"]] = data.table(level = levels, key = "level")
+    setkeyv(r$fact[[x$fact]], new.fact.keys)
+    if(isTRUE(drop)) as.cube(r)$drop() else as.cube(r)
+}
---
+++ tests/tests-rollup.R
@@ -0,0 +1,77 @@
+library(data.table)
+library(data.cube)
+
+### no hierarchy ----------------------------------------------------------
+
+set.seed(1L)
+ar.dimnames = list(color = sort(c("green","yellow","red")), 
+                   year = as.character(2011:2015), 
+                   status = sort(c("active","inactive","archived","removed")))
+ar.dim = sapply(ar.dimnames, length)
+ar = array(sample(c(rep(NA, 4), 4:7/2), prod(ar.dim), TRUE), 
+           unname(ar.dim),
+           ar.dimnames)
+
+cb = as.cube(ar)
+
+# all subtotals
+r = rollup(cb, MARGIN = c("color","year"), FUN = sum, na.rm=TRUE)
+stopifnot(
+    identical(dim(r), c(3L,5L,3L)),
+    all.equal(
+        cb$env$fact[[1L]][, .(value=sum(value))],
+        r$env$fact[[1L]][, .(value=sum(value) / nrow(r$env$dims$level))] # confirm expected double counting
+    ),
+    all.equal(unname(r$dapply(names, simplify = TRUE)), c("color","year","level")),
+    as.data.table(r)[is.na(color) & is.na(year), .N==1L],
+    as.data.table(r)[is.na(year), .N==uniqueN(color)]
+)
+
+# chosen subtotals
+r = rollup(cb, MARGIN = c("color","year"), INDEX = c(0L,2L), FUN = sum, na.rm=TRUE)
+stopifnot(
+    identical(dim(r), c(3L,5L,2L)),
+    all.equal(
+        cb$env$fact[[1L]][, .(value=sum(value))],
+        r$env$fact[[1L]][, .(value=sum(value) / nrow(r$env$dims$level))] # confirm expected double counting
+    ),
+    all.equal(unname(r$dapply(names, simplify = TRUE)), c("color","year","level")),
+    as.data.table(r)[is.na(color) & is.na(year), .N==1L]
+)
+
+# array does not handle NA dim keys - which reflects aggregate, so
+arr = as.array(r)
+stopifnot(all(is.na(arr[,,2L])))
+
+# `j` arg vs `FUN`
+r = rollup(cb, MARGIN = c("color","year"), j = .(value = sum(value, na.rm=TRUE)))
+stopifnot(all.equal(r, rollup(cb, MARGIN = c("color","year"), NULL, sum, na.rm=TRUE)))
+stopifnot(
+    identical(dim(r), c(3L,5L,3L)),
+    all.equal(
+        cb$env$fact[[1L]][, .(value=sum(value))],
+        r$env$fact[[1L]][, .(value=sum(value) / nrow(r$env$dims$level))] # confirm expected double counting
+    ),
+    all.equal(unname(r$dapply(names, simplify = TRUE)), c("color","year","level")),
+    as.data.table(r)[is.na(color) & is.na(year), .N==1L],
+    as.data.table(r)[is.na(year), .N==uniqueN(color)]
+)
+
+# format cube - sorting of NA to last and custom format per measure, see *currency* example
+rr = format(r)
+stopifnot(is.data.table(rr), rr[nrow(rr)][is.na(color) & is.na(year), .N==1L])
+# format currency from nice SO: http://stackoverflow.com/a/23833928/2490497
+printCurrency = function(value, currency.sym="$", digits=2, sep=",", decimal=".", ...) paste(currency.sym, formatC(value, format = "f", big.mark = sep, digits=digits, decimal.mark=decimal), sep="")
+stopifnot(printCurrency(123123.334)=="$123,123.33")
+rcurrency = format(r, measure.format = list(value = printCurrency))
+stopifnot(
+    is.character(rcurrency$value),
+    as.numeric(gsub("$", "", rcurrency$value, fixed = TRUE))==format(r)$value
+)
+
+### hierarchy ---------------------------------------------------------------
+
+# cb = as.cube(populate_star(1e5))
+# 
+# rollup(cb, MARGIN = c("curr_name","geog_abb"), FUN = sum)
+# rollup(cb, MARGIN = c("geog_region_name","time_year","time_quarter"), FUN = sum)

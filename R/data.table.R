ffcb3b27b0f57d2540f88d876fd864a28f470a07
jangorecki <j.gorecki@wit.edu.pl>
2016-04-30 21:23:42 +0100

    support for R 3.0.0 in terms of R different handling copies

--- R/data.table.R
+++ R/data.table.R
@@ -88,12 +88,17 @@ swap.on = function(x) {
 # join and lookup chosen columns
 lookup = function(fact, dim, cols) {
     stopifnot(haskey(dim))
-    if (missing(cols)){
+    if (missing(cols)) {
         cols = setdiff(names(dim), key(dim))
     }
     if (!length(cols)) return(TRUE)
     if (any(cols %in% names(fact))) stop(sprintf("Column name collision on lookup for '%s' columns.", paste(cols[cols %in% names(fact)], collapse=", ")))
-    fact[dim, (cols) := mget(paste0("i.", cols)), on = c(key(dim))]
+    if (base::getRversion()<="3.0.2") {
+        # R<=v3.0.2 force shallow copy here, so cannot be done by reference, required to pass tests on R 3.0.0
+        suppressWarnings(fact <- fact[dim, (cols) := mget(paste0("i.", cols)), on = c(key(dim))])
+    } else {
+        fact[dim, (cols) := mget(paste0("i.", cols)), on = c(key(dim))]
+    }
     TRUE
 }
 
